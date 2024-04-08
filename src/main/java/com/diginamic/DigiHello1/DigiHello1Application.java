2205433ac0c269d7c8ec7aacdb38aa2f39c1fc23
Josh-Newman-7 <raphael.pinson.professional@gmail.com>
2024-04-08 15:09:50 +0200

    First Commit

---
+++ src/main/java/com/diginamic/DigiHello1/DigiHello1Application.java
@@ -0,0 +1,13 @@
+package com.diginamic.DigiHello1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigiHello1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigiHello1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/diginamic/DigiHello1/DigiHello1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diginamic.DigiHello1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigiHello1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
